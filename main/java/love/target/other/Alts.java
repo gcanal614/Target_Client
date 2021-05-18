2513dd85afcbf35507354e708bb5044df9462272
Administrator <2364379488@qq.com>
2021-05-18 18:31:50 +0800

    我爱你

--- main/java/love/target/other/Alts.java
+++
@@ -1,9 +0,0 @@
-package love.target.other;
-
-public class Alts {
-/*
-[NFA][2]99kuttu@gmail.com:12As34df56gh$:TheMatronix| SW:2	2021-05-18 12:29:31 GG
-[NFA][9][Skywars]Glenifred@gmail.com:H@rgiter101:Glenifred| SW:5 MW_Coins:2     GG
-adamdarnold4@hotmail.co.uk:manchester4
-*/
-}
